a8f38a414e672c9f09c67a4b1886c4a6c55c5f62
KKGGBO <cb23036@bristol.ac.uk>
2025-05-30 16:36:05 +0800

    25.05.30

---
+++ nutrition-tracker/nutrition-tracker-service/src/main/java/com/nt/tracker/domain/dto/UserProfile.java
@@ -0,0 +1,29 @@
+package com.nt.tracker.domain.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class UserProfileDTO implements Serializable {
+    //  用户id
+    private Long userId;
+
+    // 性别（1=男，2=女）
+    private Integer gender;
+
+    // 年龄
+    private Integer age;
+
+    // 身高
+    private Double height;
+
+    // 体重
+    private Double weight;
+
+    // 目标（1=增肌，2=减脂）
+    private Integer goal;
+
+    // 活动等级
+    private Double activityLevel;
+}
